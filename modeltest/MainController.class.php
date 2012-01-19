d536c315bd1cb8520f228678d26445ec0b0c4967
Tom Peerdeman <tompeerdeman93@hotmail.com>
2012-01-19 15:36:21 +0100

    Model versie 2

--- modeltest/MainController.class.php
+++ modeltest/MainController.class.php
@@ -1,17 +1,35 @@
 <?php
 	class MainController{
 		public function __construct(){
+			//pagina beginnen
 			$this->beginPage();
+			
+			//Bijbehorende controller zoeken als geen pagina aangegeven is dan de home gebruiken
 			if(!isset($_GET['p']) || empty($_GET['p']) || $_GET['p'] == "home"){
 				$page = "home";
 			}else{
 				$page = $_GET['p'];
 			}
+			
+			//pagina naam wijzigen van naam naar NaamController
 			$page = ucfirst($page);
 			$page .= "Controller";
+			
+			//Bestand laden
 			include("pages/" . $page . ".class.php");
+			
+			//Controller aanmaken
 			$obj = new $page();
+			
+			//kijken of er een formulier gepost is zo ja, afhandelen laten door de controller
+			if($this->formPosted()){
+				$obj->handleForm();
+			}
+
+			//controller de pagina laten bouwen
 			$obj->buildPage();
+			
+			//pagina beeindigen
 			$this->endPage();
 		}
 	
@@ -22,5 +40,9 @@
 		private function endPage(){
 			echo "</body></html>";
 		}
+		
+		private function formPosted(){
+			return $_SERVER['REQUEST_METHOD'] == "POST";
+		}	
 	}
 ?>
--- modeltest/pages/TestController.class.php
+++ modeltest/pages/TestController.class.php
@@ -1,5 +1,6 @@
 <?php
-	class TestController{
+	include("BaseController.php");
+	class TestController extends BaseController{
 		public function buildPage(){
 			echo "<p>Hello world!</p>";
 		}
