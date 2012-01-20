6a709d338d56b6562024c782cab9aa2ccdbdd79d
Sidney Monk <sidney_monk_@hotmail.com>
2012-01-20 03:00:59 +0100

    productController en productenController. querry in productenController moet nog steeds gefixed worden zie docs.google

---
+++ v3.5/pages/ProductenController.class.php
@@ -0,0 +1,64 @@
+<?php
+	//Dit bestand kan alleen vanuit de index aangeroepen worden
+	if(!defined("INDEX"))die("NO INDEX!");
+	
+	class ProductenController extends BaseController{
+		public function buildPage(){
+			// x = categories.cat_id
+			$x=$_GET["cat"];
+			echo '<div id="contentcontainer">';	
+			$res = DB::$db->query("SELECT * FROM categories where cat_id = $x");
+			while($row = $res->fetch()){
+				echo "<h2>";
+				echo $row['cat_name'];
+				echo "</h2>";
+			}
+			// todo: 100% werkende query ( dit maakt geen onderscheid tussen subcategorien)
+			$res2 = DB::$db->query("SELECT products.image_path, product_name, price, product_id
+									FROM products
+									INNER JOIN categories 
+									ON products.cat_id = categories.parent_id
+									WHERE categories.cat_id = '$x'");
+			$teller = 0;
+			echo "<table><tr>";
+			while($row = $res2->fetch())
+			{
+				$imgPath = $row['image_path'];
+				echo "<td>
+					<div class='small'>
+						<table width='240px'>
+							<tr>
+								<td>";
+								
+								echo "<a href=\"?p=product&amp;id=" . $row['product_id'] . "\">";
+								echo "<img src= '$imgPath' alt='generic' width='170px' height='150px' align='left' />
+									</a>
+								</td>
+								<td>
+									CD-DVD<br />
+									<br />
+									<br />";
+									echo "&euro;" . $row['price'];
+								echo "</td>
+							</tr>
+							<tr>
+								<td>";
+									echo "<a href=\"?p=product&amp;id=" . $row['product_id'] . "\">";
+									echo $row['product_name'];								
+									echo "</a>
+								</td>
+							</tr>
+						</table>
+					</div>
+				</td>";
+				// maak rijen van 3; meer past niet
+				$teller++;
+				if ($teller%3==0)
+					echo "</tr><tr>";
+				}
+				echo "</tr></table>";
+			echo "</div>";
+		
+		}
+	}
+?>
