a30d5ae8c2fdcbb9958dd8a46f87be7ae07727f0
Ramses IJff <ramses.ijff@gmail.com>
2012-01-31 20:29:40 +0100

    this better not destroy anything again

--- v4.1/pages/WinkelwagenController.class.php
+++ v4.1/pages/WinkelwagenController.class.php
@@ -6,34 +6,33 @@ if(!defined("INDEX"))die("NO INDEX!");
 class WinkelwagenController extends BaseController{
 
 	public function buildPage(){
+		$totalcost = 0;
 		//If an option is selected
 		if(isset($_get['action'])){
 			$action = $_get['action'];
-			$productid = $_get ['id'];
-			$userid = DB::$db->query("SELECT id FROM users WHERE username=” . $this->user->username . ” LIMIT 1");
-			switch ($action){
-			case 'add':
-				DB::$db->query("INSERT INTO winkelwagen VALUES ('$productid', '$userid', '1')");
-				break;
-			case 'delete':
-				DB::$db->query("DELETE FROM winkelwagen WHERE prod_id = '$productid' AND user_id = '$userid'");
-				break;
-			case 'plus':
-				$oldnum =  DB::$db->query("SELECT amount FROM winkelwagen WHERE user_id = '$userid' AND prod_id = '$productid'");
-				DB::$db->query("DELETE FROM winkelwagen WHERE prod_id = '$productid' AND user_id = '$userid'");
-				DB::$db->query("INSERT INTO winkelwagen VALUES ('$productid', '$userid', '($oldnum + 1)')");
-				break;
-			case 'minus':
-				$oldnum =  DB::$db->query("SELECT amount FROM winkelwagen WHERE user_id = '$userid' AND prod_id = '$productid'");
-				DB::$db->query("DELETE FROM winkelwagen WHERE prod_id = '$productid' AND user_id = '$userid'");
-				DB::$db->query("INSERT INTO winkelwagen VALUES ('$productid', '$userid', '($oldnum - 1)')");
-				break;
+			$productid = $_get['id'];
+			$res = DB::$db->query("SELECT id FROM users WHERE username='".$this->user->username."' LIMIT 1");
+			while($res && $row = $res->fetch()){
+				switch ($action){
+				case 'add':
+					DB::$db->query("INSERT INTO winkelwagen VALUES (".$productid.", ".$row['id'].", '1')");
+					break;
+				case 'delete':
+					DB::$db->query("DELETE FROM winkelwagen WHERE prod_id = '$productid' AND user_id = ".$row['id']."");
+					break;
+				case 'plus':
+					DB::$db->query("UPDATE winkelwagen SET amount += 1 WHERE user_id = ".$row['id']." AND prod_id = '$productid'");
+					break;
+				case 'minus':
+					DB::$db->query("UPDATE winkelwagen SET amount -= 1 WHERE user_id = ".$row['id']." AND prod_id = '$productid'");
+					break;
+				}
 			}
 		}
 		//building the actual page
 		//sets up the top of the table
 		echo '
-			<table border="1">
+			<table border="1" cellpadding = "2">
 			<tr>
 			<th>Product name</th>
 			<th>Author</th>
@@ -43,28 +42,32 @@ class WinkelwagenController extends BaseController{
 			<th></th>
 			</tr>
 		';
-		$userid = DB::$db->query("SELECT id FROM users WHERE username=”.$this->user->username.” LIMIT 1");
-		$res1 = DB::$db->query("SELECT * FROM winkelwagen where user_id = $userid");
-		while($res1 && $row1 = $res1->fetch()){
-			//builds up the individual's shopping list
-			$res2 = DB::$db->query("SELECT * FROM products where product_id = $res1");
-			while($res2 && $row2 = $res2->fetch()){
-				echo '
-					<tr>
-					<th>'.$row['product_name'].'</th>
-					<th>'.$row['author'].'</th>
-					<th>'.$row['publisher'].'</th>
-					<th>'.$row['price'].'</th>
-					<th>'.$amount = countcart($x).'</th>
-					<th><a href="cart.php?action=minus&id='.$x.'"><img src="img/minus.png"></a><a href="cart.php?action=add&id='.$x.'"><img src="img/plus.png"></a></th>
-					</tr>
-				';
-				$totalcost += ($row['price'] * $amount);
+		$res0 = DB::$db->query("SELECT id FROM users WHERE username='".$this->user->username."' LIMIT 1");
+		while($res0 && $row0 = $res0->fetch()){
+			$res1 = DB::$db->query("SELECT * FROM winkelwagen where user_id =". $row0['id']. "");
+			while($res1 && $row1 = $res1->fetch()){
+				//builds up the individual's shopping list
+				$res2 = DB::$db->query("SELECT * FROM products where product_id = ".$row1['prod_id']."");
+				while($res2 && $row2 = $res2->fetch()){
+					echo '
+						<tr>
+						<th>'.$row2['product_name'].'</th>
+						<th>'.$row2['author'].'</th>
+						<th>'.$row2['publisher'].'</th>
+						<th>'.$row2['price'].'</th>
+						<th>'.$row1['amount'].'</th>
+						<th><a href="?p=winkelwagen&action=minus&id='.$row2['product_id'].'"><img src="img/minus.png"></a><a href="?p=winkelwagen&action=plus&id='.$row2['product_id'].'"><img src="img/plus.png"></a></th>
+						</tr>
+					';
+					$totalcost += ($row2['price'] * $row1['amount']);
 				}
-			
+			}
 		}
 		echo '
-		'
+			<td colspan="5">Total price:</td>
+			<th>&euro;'.$totalcost.'</th>
+			</table>
+		';
 	}
 }
 ?>
