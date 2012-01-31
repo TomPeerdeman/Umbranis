2885a6cb58441361f54cc813389966c26753e931
Ramses IJff <ramses.ijff@gmail.com>
2012-01-31 14:51:42 +0100

    please, for the love of a thousand succubi, work

--- v4.1/pages/WinkelwagenController.class.php
+++ v4.1/pages/WinkelwagenController.class.php
@@ -1,140 +1,70 @@
 <?php
 //Dit bestand kan alleen vanuit de index aangeroepen worden
 if(!defined("INDEX"))die("NO INDEX!");
-if(isset($_SESSION['cart'])){
-	$cart = $_SESSION['cart'];
-} 
-if(isset($_get['action'])){
-	$action = $_get['action'];
-}
+
 
 class WinkelwagenController extends BaseController{
-	
-	public function cartchange(){
-			//updates the cart;
-			switch ($action){
-				case 'add':
-					if (!empty($cart)) {
-						$cart = $cart.','.$_get['id'];
-					}
-					else{
-						$cart = $_get['id'];
-					}
-				break;
-				case 'remove':
-					$inhoud = explode(',',$cart);
-					$nextcart = '';
-					foreach ($inhoud as $x){
-						if ($x != $_get['id']){
-							if ($nextcart = ''){
-								$nextcart = $x;
-							} 
-							else{
-								$nextcart = $nextcart.''.$x;
-							}
-						} 
-						
-					}
-					$cart=$nextcart;
-				break;
-				case 'minus':
-					$inhoud = explode(',',$cart);
-					$nextcart = '';
-					$subtracted = false;
-					foreach ($inhoud as $x){
-						if ($x != $_get['id'] || ($subtracted)){ 
-							if ($nextcart = ''){
-								$nextcart = $x;
-							} 
-							else{
-								$nextcart = $nextcart.''.$x;
-							}
-						} else{
-							$subtracted = true;
-						}
-					}
-				break;
-			}
-			$_SESSION['cart'] = $cart;
-		}
-	
-	public function countcart($id){
-		$count = 0;
-		$inhoud = explode(',',$cart);
-		foreach ($inhoud as $x){
-			if ($x = $id){
-				$count += 1;
-			}
-		}
-		return $count;
-	}
-	
-	public function buildcart(){
-		if (!empty($cart)){
-			$inhoud = explode(',',$cart);
-			$builtlist = '';
-			$totalcost;
-			// sets up the top of the table
-			echo '
-				<table borders=1>
-				<tr>
-				<th>Product name</th>
-				<th>Author</th>
-				<th>Publisher</th>
-				<th>Price</th>
-				<th>Amount</th>
-				<th></th>
-				</tr>
-			';
-			//builds the list product for product
-			foreach ($inhoud as $x){
-				//checks whether this product is already present.
-				$alreadydone = false;
-				$builtarray = explode(',',$builtlist);
-				foreach ($builtarray as $y){
-					if ($x = $y) {
-						$alreadydone = true;
-					}
-				}
-				//does the actual building
-				if (!($alreadydone)){
-					$res = DB::$db->query("SELECT * FROM products where product_id = $x");
-					while($res && $row = $res->fetch()){
-						echo '
-							<tr>
-							<th>'.$row['product_name'].'</th>
-							<th>'.$row['author'].'</th>
-							<th>'.$row['publisher'].'</th>
-							<th>'.$row['price'].'</th>
-							<th>'.$amount = countcart($x).'</th>
-							<th><a href="cart.php?action=minus&id='.$x.'"><img src="img/minus.png"></a><a href="cart.php?action=add&id='.$x.'"><img src="img/plus.png"></a></th>
-							</tr>
-						';
-						$totalcost += ($row['price'] * $amount);
-						$builtlist = $builtlist.','.$x;
-					}
-				}
-			}
-			//Gives the total price.
-			echo '
-				<tr>
-				<td colspan="4">Total Price:</td>
-				<th>'.$totalcost.'</th>
-				</tr>
-			';
-		}
-		else{
-			echo 'Je winkelwagen is leeg';
-		}
-	}
-	
+
 	public function buildPage(){
-		if (isset($_GET['action'])){
-			$this->cartchange();
+		//If an option is selected
+		if(isset($_get['action'])){
+			$action = $_get['action'];
+			$productid = $_get ['id'];
+			$userid = DB::$db->query("SELECT id FROM users WHERE username=” . $this->user->username . ” LIMIT 1");
+			switch ($action){
+			case 'add':
+				DB::$db->query("INSERT INTO winkelwagen VALUES ('$productid', '$userid', '1')");
+				break;
+			case 'delete':
+				DB::$db->query("DELETE FROM winkelwagen WHERE prod_id = '$productid' AND user_id = '$userid'");
+				break;
+			case 'plus':
+				$oldnum =  DB::$db->query("SELECT amount FROM winkelwagen WHERE user_id = '$userid' AND prod_id = '$productid'");
+				DB::$db->query("DELETE FROM winkelwagen WHERE prod_id = '$productid' AND user_id = '$userid'");
+				DB::$db->query("INSERT INTO winkelwagen VALUES ('$productid', '$userid', '($oldnum + 1)')");
+				break;
+			case 'minus':
+				$oldnum =  DB::$db->query("SELECT amount FROM winkelwagen WHERE user_id = '$userid' AND prod_id = '$productid'");
+				DB::$db->query("DELETE FROM winkelwagen WHERE prod_id = '$productid' AND user_id = '$userid'");
+				DB::$db->query("INSERT INTO winkelwagen VALUES ('$productid', '$userid', '($oldnum - 1)')");
+				break;
+			}
+		}
+		//building the actual page
+		//sets up the top of the table
+		echo '
+			<table border="1">
+			<tr>
+			<th>Product name</th>
+			<th>Author</th>
+			<th>Publisher</th>
+			<th>Price</th>
+			<th>Amount</th>
+			<th></th>
+			</tr>
+		';
+		$userid = DB::$db->query("SELECT id FROM users WHERE username=”.$this->user->username.” LIMIT 1");
+		$res1 = DB::$db->query("SELECT * FROM winkelwagen where user_id = $userid");
+		while($res1 && $row1 = $res1->fetch()){
+			//builds up the individual's shopping list
+			$res2 = DB::$db->query("SELECT * FROM products where product_id = $res1");
+			while($res2 && $row2 = $res2->fetch()){
+				echo '
+					<tr>
+					<th>'.$row['product_name'].'</th>
+					<th>'.$row['author'].'</th>
+					<th>'.$row['publisher'].'</th>
+					<th>'.$row['price'].'</th>
+					<th>'.$amount = countcart($x).'</th>
+					<th><a href="cart.php?action=minus&id='.$x.'"><img src="img/minus.png"></a><a href="cart.php?action=add&id='.$x.'"><img src="img/plus.png"></a></th>
+					</tr>
+				';
+				$totalcost += ($row['price'] * $amount);
+				}
+			
 		}
 		echo '
-			<div id="fullcart">'.$this->buildcart().'</div>
-		';
+		'
 	}
 }
 ?>
