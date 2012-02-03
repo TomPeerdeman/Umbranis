074c9afad9a6c407e2710f79d7d1fd5e6bb9850e
Tom Peerdeman <tompeerdeman93@hotmail.com>
2012-02-03 19:16:09 +0100

    Topsales

--- v5/base/TopsalesController.class.php
+++ v5/base/TopsalesController.class.php
@@ -6,12 +6,16 @@
 		private $topsales;
 	
 		public function __construct($page){
-			//todo: selecteren van producten die ook gekocht werden als er een productpagina geopend is.
-			$res = DB::$db->query("SELECT product_id, product_name, price, subcat.cat_name AS subcat_name, hoofdcat.cat_name AS hoofdcat_name
-				FROM products
-				JOIN categories AS subcat ON products.cat_id = subcat.cat_id
-				JOIN categories AS hoofdcat ON subcat.parent_id = hoofdcat.cat_id
-				LIMIT 5");
+			//Selecteer de meest verkochte producten
+			$res = DB::$db->query("SELECT order_products.product_id, product_name, products.price, subcat.cat_name AS subcat_name, hoofdcat.cat_name AS hoofdcat_name
+				FROM order_products
+				JOIN products ON order_products.product_id = products.product_id
+				JOIN categories AS subcat ON products.cat_id = subcat.cat_id 
+				JOIN categories AS hoofdcat ON subcat.parent_id = hoofdcat.cat_id 
+				GROUP BY order_products.product_id 
+				ORDER BY SUM(amount) DESC 
+				LIMIT 5
+			");
 			while($row = $res->fetch()){
 				$this->topsales[] = $row;
 			}
@@ -23,8 +27,11 @@
 	<strong>Meest verkocht</strong><br />
 	<table>
 <?php
+	//BaseController wordt gebruikt voor zijn prijs methode
 	$base = new BaseController();
-	for($i = 0; $i < 5; $i++){
+	
+	//Meest verkochte producten
+	for($i = 0; $i < count($this->topsales); $i++){
 		echo '<tr>
 			<td rowspan="3" class="topnum"><a href="?p=product&amp;id=' . $this->topsales[$i]['product_id'] . '" onclick="return page_load(\'product\', \'id=' . $this->topsales[$i]['product_id'] . '\');">' . ($i + 1) . '</a></td>
 			<td class="topitemtop"><a href="?p=product&amp;id=' . $this->topsales[$i]['product_id'] . '" onclick="return page_load(\'product\', \'id=' . $this->topsales[$i]['product_id'] . '\');">' . $this->topsales[$i]['hoofdcat_name'] . ' - ' . $this->topsales[$i]['subcat_name'] . '</a></td>
@@ -38,6 +45,21 @@
 		<tr>
 			<td class="spacer" colspan="2"></td>
 		</tr>';
+	}
+	
+	//Lege items opvullen
+	if(count($this->topsales) < 5){
+		for($i = 0; $i < 5 - count($this->topsales); $i++){
+			echo '<tr>
+				<td rowspan="2" class="topnum"><a href="?=home">' . ($i + 1 + count($this->topsales)) . '</a></td>
+			</tr>
+			<tr>
+				<td class="topitemtop"><a href="?=home">Nog geen product</a></td>
+			</tr>		
+			<tr>
+				<td class="spacer" colspan="2"></td>
+			</tr>';
+		}
 	}
 ?>
 	</table>
@@ -59,8 +81,7 @@
 		tweets: {
 		  background: '#000000',
 		  color: '#ffffff',
-		  //links: '#4aed05'
-		  links: 'rgb(248, 213, 12)'
+		  links: '#F8D50C'
 		}
 	  },
 	  features: {
