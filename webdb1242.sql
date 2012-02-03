da2023b97e15c1cec2ad9deaa4ab36e1f68021ef
Ramses IJff <ramses.ijff@gmail.com>
2012-02-03 21:32:14 +0100

    descriptions

--- webdb1242.sql
+++ webdb1242.sql
@@ -148,6 +148,12 @@ CREATE TABLE IF NOT EXISTS `password_requests` (
 -- Tabelstructuur voor tabel `products`
 --
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `products`
+--
+
 CREATE TABLE IF NOT EXISTS `products` (
   `product_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `cat_id` int(11) NOT NULL,
@@ -167,27 +173,27 @@ CREATE TABLE IF NOT EXISTS `products` (
 ) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=18 ;
 
 --
--- Gegevens worden uitgevoerd voor tabel `products`
+-- Dumping data for table `products`
 --
 
 INSERT INTO `products` (`product_id`, `cat_id`, `product_name`, `normal_price`, `price`, `stock`, `delivery_time`, `publisher`, `author`, `image_path`, `description`, `ean_code`, `date`) VALUES
-(1, 13, 'The elder Scrolls: Skyrim', 49.99, 49.99, 91, 0, 'Bethesda Softworks', '', 'skyrim.png', '', '1234567891012', '2012-01-18 17:04:24'),
-(2, 13, 'Portal 2', 59.99, 59.99, 7, 2, 'Valve Corporation', '', 'portal.png', '', '1234567891013', '2012-01-18 17:04:24'),
-(3, 5, 'Birds of Fire', 15.99, 15.99, 10, 1, 'Sony music entertainment', 'Mahavishnu Orchestra', '', '', '4209714987224', '2012-01-18 18:36:19'),
-(4, 5, 'Unrecognizable screeches from a ', 15.99, 10.99, 10, 1, '', 'Ramses Ijff', '', '', '4209714987125', '2012-01-18 18:36:54'),
-(5, 9, 'Final destination 5', 22.99, 22.99, 0, 15, 'Warner Home Video', 'Steven Quale', 'final_destination_5.png', 'omschrijving', '5051888087602', '2012-01-20 12:47:46'),
-(6, 6, 'Got My Mojo Working', 9.99, 9.99, 22, 2, '', 'Muddy Waters', 'mojo_working.png', '', '7854269875421', '2012-02-03 15:54:46'),
-(7, 7, 'In the Navy', 12.99, 12.99, 23, 7, 'Can''t stop productions', 'the village people', 'in_the_navy.png', '', '4587963215478', '2012-02-03 16:08:03'),
-(8, 8, 'While heaven wept', 4.99, 4.99, 55, 616, 'nuclear blast records', 'fear of infinity', 'while_heaven_wept.png', '', '6166166616616', '2012-02-03 16:22:35'),
-(9, 10, 'Robowar', 1.99, 1.99, 50, 10, '', 'Bruno Mattei', 'robowar.png', '', '7784512568974', '2012-02-03 16:25:14'),
-(10, 11, 'Cool as Ice', 21.99, 21.99, 54, 1, 'Universal pictures', '', 'cool_as_ice.png', '', '4551245357849', '2012-02-03 16:28:46'),
-(11, 12, 'I, Robot', 12.99, 12.99, 5, 11, 'Davis Entertainment', '', 'i_robot.png\n', '', '4785462189758', '2012-02-03 16:34:43'),
-(12, 14, 'call_of_duty_2', 45.44, 34.34, 8, 1111, 'Activision', '', 'call_of_duty_2.png', '', '4587621598743', '2012-02-03 16:46:30'),
-(13, 15, 'Ghost Recon: Ghost Soldier', 35.99, 35.99, 6, 7, 'Red Storm Entertainment', 'Tom Clancy', 'ghost_recon_future.png', '', '8759426589745', '2012-02-03 16:49:30'),
-(14, 16, 'a Boy and his Blob', 13.99, 12.99, 3, 22, 'WayForward Technologies', '', 'boy_and_his_blob.png', '', '1254789547854', '2012-02-03 16:52:46'),
-(15, 17, 'Forever Odd', 11.99, 10.99, 1, 1, '', 'Dean Koontz', 'forever_odd.png', '', '3562897456987', '2012-02-03 16:58:25'),
-(16, 18, 'Het verdriet van belgië', 23.33, 12.11, 2, 11, 'De bezige bij', 'Hugo Claus', 'verdriet_van_belgie.png', '', '7854968759784', '2012-02-03 17:09:50'),
-(17, 19, 'Linear and Geometric Algebra', 21.99, 21.99, 8, 66, '', 'Alan Macdonald', 'linear_geometric_algebra.png', '', '2365412546321', '2012-02-03 17:11:58');
+(1, 13, 'The elder Scrolls: Skyrim', 49.99, 49.99, 10, 0, 'Bethesda Softworks', 'Todd Howard', 'skyrim.png', 'Het lang verwachte vervolg op Oblivion. Twee-honderd jaar na de demonische invasie wordt het land op nieuw bedreigd door de herrezen draken en de recente alliantie van elven. Alleen de Dovahkin, de mythische drakenstrijder, kan het land nog redden, maar hij is gevangen genomen bij het oversteken van de grens...', '1234567891012', '2012-01-18 17:04:24'),
+(2, 13, 'Portal 2', 59.99, 59.99, 10, 2, 'Valve Corporation', 'Joshua Weier', 'portal.png', 'GlaDoS is terug, en klaar voor wraak.\r\n\r\nHonderden jaren zijn voorbij gegaan sinds de gebeurtenissen van de oorspronkelijke portal, en Aperture is in verval geraakt. Maar als GlaDoS weer wakker wordt, moet Chell op avontuur door de diepste niveaus van de laboratoria, waar vele nieuwe puzzels voor haar klaar liggen.', '1234567891013', '2012-01-18 17:04:24'),
+(3, 5, 'Birds of Fire', 15.99, 15.99, 10, 1, 'Sony music entertainment', 'Mahavishnu Orchestra', 'birds_of_fire.png', 'Een van de beroemde eerste platen van het mahavishnu orkest, nu voor het eerst uitgebracht op CD! Het beste van de jaren zeventig in uw huis.', '4209714987224', '2012-01-18 18:36:19'),
+(4, 5, 'Unrecognizable screeches from a ', 15.99, 10.99, 10, 1, 'Gritmoaner productions', 'Ramses Ijff', 'no_image.png', 'There... There could be no description. It does not belong.', '4209714987125', '2012-01-18 18:36:54'),
+(5, 9, 'Final destination 5', 22.99, 22.99, 0, 15, 'Warner Home Video', 'Steven Quale', 'final_destination_5.png', 'Het leven van vijf mensen is gered, maar voor hoe lang? Een epische thriller, vol griezelen, spanning en plottwists. ', '5051888087602', '2012-01-20 12:47:46'),
+(6, 6, 'Got My Mojo Working', 9.99, 9.99, 22, 2, 'Chess Records', 'Muddy Waters', 'mojo_working.png', 'De vader des blues-muziek in zijn meest beroemde album. Mis dit stukje wereld-geschiedenis niet!', '7854269875421', '2012-02-03 16:54:46'),
+(7, 7, 'In the Navy', 12.99, 12.99, 23, 7, 'Can''t stop productions', 'the village people', 'in_the_navy.png', 'De wereldberoemde band the village people in hun meest bekende nummers. ', '4587963215478', '2012-02-03 17:08:03'),
+(8, 8, 'While heaven wept', 4.99, 4.99, 55, 616, 'nuclear blast records', 'fear of infinity', 'while_heaven_wept.png', 'De amerikaanse metal-band brengt hun opus uit. Donderende muziek geeft de strijd tegen de hemel en het heelal zelf vorm.', '6166166616616', '2012-02-03 17:22:35'),
+(9, 10, 'Robowar', 1.99, 1.99, 50, 10, 'Flora Film', 'Bruno Mattei', 'robowar.png', 'Actie-star Bruno Mattei neemt het op tegen een losgebroken superwapen. Actie, spanning en romance, deze film heeft alles voor een uitstekende avond.', '7784512568974', '2012-02-03 17:25:14'),
+(10, 11, 'Cool as Ice', 21.99, 21.99, 54, 1, 'Universal pictures', 'David Kellogg', 'cool_as_ice.png', 'Vanilla Ice in zijn eerste film brengt ons een nieuwe kijk in zijn karakter en zijn relaties. Een must-see voor elke fan van Ice!', '4551245357849', '2012-02-03 17:28:46'),
+(11, 12, 'I, Robot', 12.99, 12.99, 5, 11, 'Davis Entertainment', 'Alex Proyas', 'i_robot.png\n', 'Isaac Asimov''s visie van de toekomst komt tot leven in deze science-fiction actie-thriller. Zullen de robots de mensheid overwelmen, of brengt een robot ze juist vrijheid?', '4785462189758', '2012-02-03 17:34:43'),
+(12, 14, 'Call of Duty 2', 45.44, 34.34, 9, 1111, 'Activision', 'Keith Arem', 'call_of_duty_2.png', 'Ook jij kan nu ten strijde trekken tegen de nazi''s in het lang-verwachte volgende deel in de Call of Duty serie. ', '4587621598743', '2012-02-03 17:46:30'),
+(13, 15, 'Ghost Recon: Future Soldier', 35.99, 35.99, 6, 7, 'Red Storm Entertainment', 'Tom Clancy', 'ghost_recon_future.png', 'In een toekomst waar rusland de wereld heeft overgenomen, is er maar een kans op vrijheid. Kozak, Pepper, 30k en Bones vormen de laatste hoop om deze onderdrukker te overweldigen. Maar zijn zij bevrijders, of slechts nieuwe onderdrukkers?', '8759426589745', '2012-02-03 17:49:30'),
+(14, 16, 'a Boy and his Blob', 13.99, 12.99, 3, 22, 'WayForward Technologies', 'Sean Velasco', 'boy_and_his_blob.png', 'Een avontuur voor kinderen van alle leeftijden. Een simpel, doch ontroerend verhaal brengt gegarandeerd plezier voor alle spelers.', '1254789547854', '2012-02-03 17:52:46'),
+(15, 17, 'Forever Odd', 11.99, 10.99, 2, 1, 'Bantam Publishing', 'Dean Koontz', 'forever_odd.png', 'Het vierde deel in de Odd Thomas serie. Een vrouw geobsedeerd door de psychische krachten van Thomas heeft Danny ontvoerd. Kan Thomas hem redden? En waarom is deze vrouw zo geobsedeerd door geesten?', '3562897456987', '2012-02-03 17:58:25'),
+(16, 18, 'Het verdriet van belgië', 23.33, 12.11, 2, 11, 'De bezige bij', 'Hugo Claus', 'verdriet_van_belgie.png', 'Het Magnum Opus van Hugo Claus, over een familie in de tweede wereldoorlog. Leugenaars en collaborateurs, voor hun is de oorlog niet het grootste gevaar.', '7854968759784', '2012-02-03 18:09:50'),
+(17, 19, 'Linear and Geometric Algebra', 21.99, 21.99, 8, 66, 'Niet van Toepassing', 'Alan Macdonald', 'linear_geometric_algebra.png', 'Een niet te missen gids voor de lineare en geometrische algebra. Gebruikt door tientallen universiteiten over de hele wereld. Verrijk uw kennis nu!', '2365412546321', '2012-02-03 18:11:58');
 
 -- --------------------------------------------------------
 
